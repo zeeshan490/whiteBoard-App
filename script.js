b759cf4bf2c81700acff6c2cedf906e8e680ad64
zeeshan ajmal <zeeshanajmal8@gmail.com>
2021-08-01 20:30:48 +0530

    tools icon added

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ window.addEventListener("resize", function () {
 
 let ctx = canvas.getContext('2d');
 
+let isPenDown = false;
+
+canvas.addEventListener('mousedown' , function(e){
+    isPenDown =true;
+    let x = e.clientX;
+    let y = e.clientY - 100;
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+})
+
+canvas.addEventListener('mousemove', function(e){
+    if(isPenDown){
+        let x = e.clientX;
+        let y = e.clientY - 100;
+        ctx.lineTo(x,y);
+        ctx.stroke();
+       
+    }
+})
+
+canvas.addEventListener('mouseup' , function(e){
+    isPenDown = false;
+})
+
