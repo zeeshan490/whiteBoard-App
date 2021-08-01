3f2308c1ec4a559147aaf7259c8fc2f52818e62d
zeeshan ajmal <zeeshanajmal8@gmail.com>
2021-08-02 02:24:11 +0530

    undo & redo implemented

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
-let canvas = document.querySelector('#canvas');
-
+let canvas = document.querySelector("#canvas");
 canvas.width = window.innerWidth;
-canvas.Height = window.innerHeight - 100;
-
+canvas.height = window.innerHeight - 100;
 window.addEventListener("resize", function () {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight - 100;
-  });
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight - 100;
+});
+// canvas drawing gets erased on window resize ???
+// a context object which provides fun for 2d drawing
+let ctx = canvas.getContext("2d");
 
-let ctx = canvas.getContext('2d');
 
+let linesDB = [];
+let redoLinesDB =[];
 let isPenDown = false;
+let line = [];
 
-canvas.addEventListener('mousedown' , function(e){
-    isPenDown =true;
+canvas.addEventListener("mousedown" , function(e){
+    if(redoLinesDB.length){
+        redoLinesDB=[];
+    }
+    console.log("Inside mouse down");
+    isPenDown = true;
     let x = e.clientX;
-    let y = e.clientY - 100;
+    let y = e.clientY-100;
     ctx.beginPath();
-    ctx.moveTo(x,y);
+    ctx.moveTo(x , y);
+
+    let pointObject = {
+        x:x , 
+        y:y , 
+        type:"md"
+    }
+    line.push(pointObject);
 })
 
-canvas.addEventListener('mousemove', function(e){
+canvas.addEventListener("mousemove" , function(e){
     if(isPenDown){
+        console.log("Inside mousemove")
         let x = e.clientX;
-        let y = e.clientY - 100;
-        ctx.lineTo(x,y);
+        let y = e.clientY-100;
+        ctx.lineTo(x , y);
         ctx.stroke();
-       
+
+        let pointObject = {
+            x:x , 
+            y:y , 
+            type:"mm"
+        }
+        line.push(pointObject);
     }
 })
 
-canvas.addEventListener('mouseup' , function(e){
+canvas.addEventListener("mouseup" , function(e){
+    console.log("mouseup");
     isPenDown = false;
-})
 
+    linesDB.push(line);
+    line = [];
+
+    console.log(linesDB);
+})
